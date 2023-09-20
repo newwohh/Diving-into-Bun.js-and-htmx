61729a60eecfff875bda467ddab1cb17c37a005e
newwohh <nevoznvx@gmail.com>
2023-09-20 17:26:22 +0530

    new-todo-bun&htmx-project

---
+++ htmx/bun-example-app/index.tsx
@@ -0,0 +1,50 @@
+import { renderToString } from "react-dom/server";
+
+const server = Bun.serve({
+  hostname: "localhost",
+  port: 8080,
+  fetch: handler,
+});
+
+type Todo = { id: number; text: string };
+
+const todos: Todo[] = [];
+
+console.log("Listening on " + server.hostname + server.port);
+
+async function handler(request: Request): Promise<Response> {
+  const url = new URL(request.url);
+
+  if (url.pathname === "/" || url.pathname === "") {
+    return new Response(Bun.file("index.html"));
+  }
+
+  if (url.pathname === "/todos" && request.method === "POST") {
+    const { todo } = await request.json();
+
+    if (!todo?.length) return new Response("bad request", { status: 400 });
+
+    todos.push({
+      id: todos.length + 1,
+      text: todo,
+    });
+
+    return new Response(renderToString(<TodoList todos={todos} />));
+  }
+
+  if (url.pathname === "/todos" && request.method === "GET") {
+    return new Response(renderToString(<TodoList todos={[]} />));
+  }
+
+  return new Response("not found", { status: 404 });
+}
+
+function TodoList(props: { todos: Todo[] }) {
+  return (
+    <ul>
+      {props.todos.length
+        ? props.todos.map((todo) => <li key={todo.id}>{todo.text}</li>)
+        : "No todos"}
+    </ul>
+  );
+}
